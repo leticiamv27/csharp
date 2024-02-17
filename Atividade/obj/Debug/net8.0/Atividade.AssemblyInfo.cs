bb67d3cd70cbb2b8ce0c0e6f302d885ad2e1f857
leticiamv27 <leticiamv@gmail.com>
2024-02-17 10:24:17 -0300

    segundo commit

--- Atividade/obj/Debug/net8.0/Atividade.AssemblyInfo.cs
+++ Atividade/obj/Debug/net8.0/Atividade.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90eda7822f31fbe6d048baffdbdcafdae4ad1746")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
